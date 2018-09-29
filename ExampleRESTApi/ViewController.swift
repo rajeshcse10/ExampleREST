c4f0e80f3c3880649f71675fae781b231998308b
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2018-09-29 20:05:02 +0600

    Initial Commit

---
+++ ExampleRESTApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleRESTApi
+//
+//  Created by Rajesh Karmaker on 29/9/18.
+//  Copyright © 2018 Rajesh Karmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
